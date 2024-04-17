80d06113bf52ac6eb8c27d39487fea5953de6961
mahabube <info.freelancermahabub@gmail.com>
2024-04-17 09:08:48 +0600

    first commit

---
+++ arrau_input_output_1.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main(){
+    int ar[3];
+    return 0;
+}
---
+++ sum_of_array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(){
+    // int n;
+    // scanf("%d", &n);
+    // int ar[n];
+    // for (int i = 0; i < n; i++)
+    // {
+    //   scanf("%d", &ar[i]);
+    // }
+
+    // int sum = 0;
+    // for (int i = 0; i < n; i++)
+    // {
+    //    sum = sum+ar[i];
+    // }
+    // printf("%d", sum);
+    int arrayName[] = {1, 2, 3};
+printf("%d", arrayName[3]);
+    
+    return 0;
+}
